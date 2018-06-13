13d05e357254092a419e60c58c2cdf2a938d4eb6
Ilia Bozhinov <ammen99@gmail.com>
2018-06-13 16:35:10 +0300

    update wayfire-shell-panel

--- shell/panel/main.cpp
+++ shell/panel/main.cpp
@@ -5,73 +5,6 @@
 #include <getopt.h>
 #include <signal.h>
 
-struct wayfire_shell_output {
-    wayfire_panel* panel;
-};
-
-wayfire_config *config;
-
-std::map<uint32_t, wayfire_shell_output> outputs;
-
-void output_created_cb(void *data, wayfire_shell *wayfire_shell,
-        uint32_t output, uint32_t width, uint32_t height)
-{
-    outputs[output].panel = new wayfire_panel(config, output, width, height);
-    //wayfire_shell_output_fade_in_start(wayfire_shell, output);
-}
-
-void output_resized_cb(void *data, wayfire_shell *wayfire_shell,
-        uint32_t output, uint32_t width, uint32_t height)
-{
-    auto it = outputs.find(output);
-    if (it == outputs.end())
-        return;
-
-    if (it->second.panel)
-        it->second.panel->resize(width, height);
-}
-
-void output_destroyed_cb(void *data, wayfire_shell *wayfire_shell, uint32_t output)
-{
-    auto it = outputs.find(output);
-
-    if (it == outputs.end())
-        return;
-
-    if (it->second.panel)
-        delete it->second.panel;
-    if (it->second.gamma)
-        delete it->second.gamma;
-}
-
-void output_autohide_panels_cb(void *data, wayfire_shell *wayfire_shell, uint32_t output, uint32_t autohide)
-{
-    auto it = outputs.find(output);
-
-    if (it == outputs.end())
-        return;
-
-    if (it->second.panel)
-        it->second.panel->set_autohide(autohide);
-}
-
-bool gamma_adjust_enabled;
-
-void output_gamma_size_cb(void *data, wayfire_shell *shell, uint32_t output,
-        uint32_t size)
-{
-    if (size > 0 && gamma_adjust_enabled)
-        outputs[output].gamma = new gamma_adjust(output, size, config);
-}
-
-static const struct wayfire_shell_listener bg_shell_listener = {
-    .output_created = output_created_cb,
-    .output_resized = output_resized_cb,
-    .output_destroyed = output_destroyed_cb,
-    .output_autohide_panels = output_autohide_panels_cb,
-    .gamma_size = output_gamma_size_cb,
-};
-
 int main(int argc, char *argv[])
 {
     std::string home_dir = secure_getenv("HOME");
@@ -95,27 +28,16 @@ int main(int argc, char *argv[])
         }
     }
 
-    config = new wayfire_config(config_file);
- //   auto section = config->get_section("shell");
-
-//    gamma_adjust_enabled = section->get_int("color_temp_enabled", 0);
-
-    if (!setup_wayland_connection())
-        return -1;
-
-    wayfire_shell_add_listener(display.wfshell, &bg_shell_listener, 0);
+    auto config = new wayfire_config(config_file);
+    auto display = new wayfire_display([=] (wayfire_output *output)
+    {
+        new wayfire_panel(config, output);
+    });
 
     while(true) {
-        if (wl_display_dispatch(display.wl_disp) < 0)
+        if (wl_display_dispatch(display->display) < 0)
             break;
     }
 
-    for (auto x : outputs) {
-        if (x.second.panel)
-            delete x.second.panel;
-        if (x.second.gamma)
-            delete x.second.gamma;
-    }
-
-    finish_wayland_connection();
+    delete display;
 }
