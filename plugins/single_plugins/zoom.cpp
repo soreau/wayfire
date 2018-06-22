37b171878ccb5fe1bb47fec9a487d9ec82171d7f
Scott Moreau <oreaus@gmail.com>
2018-06-22 01:49:04 -0600

    zoom: Implement animated transitions

--- plugins/single_plugins/zoom.cpp
+++ plugins/single_plugins/zoom.cpp
@@ -3,6 +3,7 @@
 #include <opengl.hpp>
 #include <debug.hpp>
 #include <render-manager.hpp>
+#include <animation.hpp>
 
 class wayfire_invert_screen : public wayfire_plugin_t
 {
@@ -16,7 +17,9 @@ class wayfire_invert_screen : public wayfire_plugin_t
     GLuint program, posID, uvID;
 
     float zoom = 1.0;
+    float target = 1.0;
     bool hook_set = false;
+    wf_duration duration;
 
     public:
         void init(wayfire_config *config)
@@ -29,40 +32,36 @@ class wayfire_invert_screen : public wayfire_plugin_t
             axis = [=] (wlr_event_pointer_axis* ev)
             {
                 if (ev->orientation == WLR_AXIS_ORIENTATION_VERTICAL)
-                    update_zoom(ev->delta);
+                    update_zoom_target(ev->delta);
             };
 
+            duration = wf_duration(new_static_option("300"), wf_animation::linear);
             auto section = config->get_section("zoom");
             modifier = section->get_option("modifier", "<super>");
-            speed    = section->get_option("speed", "0.1");
+            speed    = section->get_option("speed", "0.005");
 
             output->add_axis(modifier, &axis);
         }
 
-        void update_zoom(float delta)
+        void update_zoom_target(float delta)
         {
-            zoom += delta * speed->as_cached_double();
-            zoom = std::max(zoom, 1.0f);
-            zoom = std::min(zoom, 50.0f);
+            target += target * delta * speed->as_cached_double() * -1;
+            target = std::max(target, 1.0f);
+            target = std::min(target, 50.0f);
 
-            if (zoom - 1 <= 0.01)
+            if (!hook_set && target != zoom)
             {
-                output->render->rem_post(&hook);
-                hook_set = false;
-            } else
-            {
-                if (!hook_set)
-                {
-                    hook_set = true;
-                    output->render->add_post(&hook);
-                }
+                hook_set = true;
+                output->render->add_post(&hook);
             }
+            duration.start(zoom, target);
         }
 
         void render(uint32_t fb, uint32_t tex, uint32_t target)
         {
             auto w = output->handle->width;
             auto h = output->handle->height;
+            zoom = duration.progress();
 
             GetTuple(x, y, output->get_cursor_position());
 
@@ -87,6 +86,11 @@ class wayfire_invert_screen : public wayfire_plugin_t
 
             GL_CALL(glBlitFramebuffer(x1, y1, x1 + tw, y1 + th, 0, 0, w, h, GL_COLOR_BUFFER_BIT, GL_LINEAR));
             GL_CALL(glBindFramebuffer(GL_FRAMEBUFFER, 0));
+            if (!duration.running() && zoom - 1 <= 0.01)
+            {
+                output->render->rem_post(&hook);
+                hook_set = false;
+            }
         }
 };
 
